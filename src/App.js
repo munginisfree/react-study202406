fecbd72da1c6ed990b46d22a0c0bd423778d4539
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-06-13 12:43:22 +0900

    filtering with ascyn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import ExpenseList from "./components/expenses/ExpenseList";
-import NewExpense from "./components/new-expense/NewExpense";
+import ExpenseList from './components/expenses/ExpenseList';
+import NewExpense from './components/new-expense/NewExpense';
 
 const App = () => {
 
@@ -15,27 +15,40 @@ const App = () => {
         {
             title: '족발먹음',
             price: 40000,
-            date: new Date(2024, 6 - 1, 7)
+            date: new Date(2023, 12 - 1, 17)
         },
         {
             title: '헬스장등록',
             price: 300000,
             date: new Date(2024, 6 - 1, 12)
         },
+        {
+            title: '파파존스피자',
+            price: 40000,
+            date: new Date(2022, 3 - 1, 4)
+        },
+        {
+            title: '파리채',
+            price: 1500,
+            date: new Date(2023, 5 - 1, 6)
+        },
     ];
+
 
     // ExpenseForm에게 내려보낼 함수
     const onAddExpense = (userInput) => {
-        console.log('App.js가 내려보낸 함수 호출!');
+        console.log('App.js 가 내려보낸 함수 호출!');
+        // console.log(userInput);
+        expenses.push(userInput);
+        console.log(expenses);
     };
 
-   return(
-       <>
-            <NewExpense onSave={onAddExpense}/>
-            <ExpenseList expenses={expenses}/>
-
-       </>
-   );
+    return (
+        <>
+            <NewExpense onSave={onAddExpense} />
+            <ExpenseList expenses={expenses} />
+        </>
+    );
 };
 
 export default App;
