c9ca001dd0eecd737ab0c52befc44faf44ff931c
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-06-14 11:45:49 +0900

    course item

---
+++ src/components/CourseGoals/CourseItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./CourseItem.css";
+const CourseItem = ({item}) => {
+    return (
+        <li className="goal-item">{item.text}</li>
+    );
+};
+
+export default CourseItem;
