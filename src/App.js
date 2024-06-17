f84a8f1d0045efb4810afbed9d8c258018e0d4ea
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-06-17 12:25:53 +0900

    useRef

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
-import React, {useState} from 'react';
-import CourseInput from './components/CourseGoals/CourseInput';
-import CourseList from './components/CourseGoals/CourseList';
+import React, {Fragment, useRef, useState} from 'react';
 import './App.css';
+import AddUsers from './components/Users/AddUsers';
+import UserList from './components/Users/UserList';
+import ErrorModal from "./components/UI/Modal/ErrorModal";
 
-// 기본 더미 데이터
-const DUMMY_DATA = [
-    {
-        id: 'g1',
-        text: '리액트 컴포넌트 스타일링 마스터하기'
-    },
-    {
-        id: 'g2',
-        text: 'UI/UX 프로그래밍 고수되기'
-    },
-];
 
 const App = () => {
 
-    const [goals, setGoals] = useState(DUMMY_DATA);
-    // courseInput에게 전달할 함수
-    const addGoalHandler = (goalObject) => {
-        setGoals([...goals, goalObject]);
-    };
+    // 회원들이 저장될 배열
+    const [userList, setUserList] = useState([]);
 
-    // CourseItem에게 전달할함수
-    const deleteGoalHandler = (id) => {
-        console.log("id: ", id);
-        // goals.splice(goals.findIndex(g=>g.id === id), 1);
-        setGoals(goals.filter(g => g.id !== id));
+    const count = useRef(1);
+
+    const addUserHandler = user => {
+        console.log("c:", count);
+        count.current++;
+        console.log("cc: ", count.current);
+        console.log(user);
+        setUserList(prev => [
+            ...prev,
+            {
+                ...user,
+                id: Math.random().toString()
+            }
+        ]);
     };
 
     return (
-        <div>
-            <section id="goal-form">
-                <CourseInput onAdd={addGoalHandler}/>
-            </section>
-            <section id="goals">
-                <CourseList items={goals} onDelete={deleteGoalHandler}/>
-            </section>
-        </div>
+        <>
+            <AddUsers onAddUser={addUserHandler} />
+            <UserList users={userList} />
+        </>
     );
 };
 
