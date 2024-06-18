168e29a38871fe132f429dfed10f9780855b7683
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-06-18 09:30:11 +0900

    # 로그인 기능 주석

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import React, {Fragment, useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
-import AddUsers from './components/Users/AddUsers';
-import UserList from './components/Users/UserList';
-import ErrorModal from "./components/UI/Modal/ErrorModal";
+import MainHeader from "./components/SideEffect/MainHeader";
+import Login from "./components/SideEffect/Login";
+import Home from "./components/SideEffect/Home";
 
 
 const App = () => {
 
-    // 회원들이 저장될 배열
-    const [userList, setUserList] = useState([]);
-
-    const count = useRef(1);
-
-    const addUserHandler = user => {
-        console.log("c:", count);
-        count.current++;
-        console.log("cc: ", count.current);
-        console.log(user);
-        setUserList(prev => [
-            ...prev,
-            {
-                ...user,
-                id: Math.random().toString()
-            }
-        ]);
-    };
-
     return (
         <>
-            <AddUsers onAddUser={addUserHandler} />
-            <UserList users={userList} />
+            <MainHeader/>
+            <main>
+                {/*<Home/>*/}
+                <Login/>
+            </main>
         </>
     );
 };
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -37,9 +37,13 @@ import styles from './Button.module.css';
 //     `
 // ;
 
-const Button = ({ type, onClick, children}) => {
+const Button = ({ type, onClick, className, disabled, children}) => {
     return (
-        <button type={type} className={styles.button} onClick={onClick}>
+        <button type={type || "button"}
+                className={`${styles.button} ${className}`}
+                onClick={onClick}
+                disabled={disabled}
+        >
             {children}
         </button>
     );
